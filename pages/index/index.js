c88692131cdc4cf05df5b07b112f5b820f25a935
jove.wang <jove.wang@163.com>
2019-04-26 10:36:24 +0800

    v0.0.0

---
+++ pages/index/index.js
@@ -0,0 +1,85 @@
+//index.js
+
+import {TTTClient} from '../../lib/miniapp-sdk-3t';
+
+//获取应用实例
+const app = getApp()
+
+let _miniappSdk = null
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  bindJoin: function() {
+    if (!!_miniappSdk)
+      _miniappSdk.join({
+		  roomId: 123456,
+		  userId: 654321,
+		  onSuccess: () => {
+			  console.log('login OK')
+		  },
+		  onFailure: (err) => {
+			  console.log('login Failed')
+		  }
+		});
+  },
+  bindLeave: function() {
+    console.log('bindLeave _miniappSdk: ', _miniappSdk)
+    if (!!_miniappSdk)
+      _miniappSdk.leave()
+  },
+  onLoad: function () {
+	  //
+	  console.log('TTTClient:', TTTClient);
+
+	  _miniappSdk = new TTTClient({appId: 'appId', userId: 654321});
+    if (!!_miniappSdk)
+	    _miniappSdk.init({appId: 'appId', userId: 654321});
+	  //
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
